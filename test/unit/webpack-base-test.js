872b9cb2129ab994a82e938adb70ab086943fde5
Louis <cj654eji618@gmail.com>
2020-02-26 11:00:13 +0800

    test: webpack test

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,10 +4,8 @@ describe("webpack.base.js test case", () => {
 
   const baseConfig = require("../../lib/webpack.base.js");
 
-  console.log(baseConfig);
-
   it("entry", () => {
     assert.equal(baseConfig.entry.index.indexOf("builder-webpack/test/smoke/template/src/index/index.js") > -1, true);
-    assert.equal(baseConfig.entry.search.indexOf("builder-webpack/test/smoke/template/src/search/index.js"));
+    assert.equal(baseConfig.entry.search.indexOf("builder-webpack/test/smoke/template/src/search/index.js" > -1, true));
   })
 })
